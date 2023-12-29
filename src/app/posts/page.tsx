bdf59f9342a408891f688d0f34d3eba800bdaecd
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-12-29 14:31:11 +0700

    create pagination and list all of posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,21 @@
+import AllPosts from "@/components/pages/posts/all-posts";
+
 const Page = () => {
-  return <div>Posts</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1 items-center justify-center">
+        <h1 className="text-2xl font-bold">
+          Find your favorite post on{" "}
+          <span className="text-blue-600">Blogpedia</span> 🎉
+        </h1>
+        <p className="text-gray-400">
+          This is a simple blog app built with Next.js and Tailwind CSS.
+        </p>
+      </div>
+
+      <AllPosts />
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/components/pages/posts/all-posts.tsx
@@ -0,0 +1,126 @@
+"use client";
+import PostCard from "@/components/post-card";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { PostType, getAllPosts } from "@/lib/api";
+import { ChevronLeft, ChevronRight, ChevronsUp } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const AllPosts = () => {
+  const [posts, setPosts] = useState<PostType[] | []>([]);
+  const [isNextPage, setIsNextPage] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const getAllData = async () => {
+    setLoading(true);
+    const res = await getAllPosts({
+      page,
+      per_page: 10,
+    });
+    const nextPage = await getAllPosts({
+      page: page + 1,
+      per_page: 10,
+    });
+    setIsNextPage(nextPage.data.length > 0);
+    setPosts(res.data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getAllData();
+  }, [page]);
+
+  if (loading) {
+    return (
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4">
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+        <Skeleton className="h-screen max-h-[150px] w-full rounded-md" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-end gap-2">
+        {page > 1 && (
+          <Button variant={"ghost"} onClick={() => setPage(page - 1)}>
+            <ChevronLeft className="w-4 h-4" />
+          </Button>
+        )}
+
+        {page - 1 >= 1 && (
+          <Button variant={"ghost"} onClick={() => setPage(page - 1)}>
+            {page - 1}
+          </Button>
+        )}
+        <p className="h-10 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background bg-accent text-accent-foreground">
+          {page}
+        </p>
+
+        {isNextPage && (
+          <Button variant={"ghost"} onClick={() => setPage(page + 1)}>
+            {page + 1}
+          </Button>
+        )}
+
+        {isNextPage && (
+          <Button variant={"ghost"} onClick={() => setPage(page + 1)}>
+            <ChevronRight className="w-4 h-4" />
+          </Button>
+        )}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4">
+        {posts.map((post) => (
+          <Link
+            href={`/posts/${post.id}`}
+            key={post.id}
+            className="flex items-center justify-center w-full hover:scale-105 transition-all duration-300"
+          >
+            <PostCard data={post} key={post.id} />
+          </Link>
+        ))}
+      </div>
+      <div className="flex items-center justify-end gap-2">
+        {page > 1 && (
+          <Button variant={"ghost"} onClick={() => setPage(page - 1)}>
+            <ChevronLeft className="w-4 h-4" />
+          </Button>
+        )}
+
+        {page - 1 >= 1 && (
+          <Button variant={"ghost"} onClick={() => setPage(page - 1)}>
+            {page - 1}
+          </Button>
+        )}
+        <p className="h-10 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background bg-accent text-accent-foreground">
+          {page}
+        </p>
+
+        {isNextPage && (
+          <Button variant={"ghost"} onClick={() => setPage(page + 1)}>
+            {page + 1}
+          </Button>
+        )}
+
+        {isNextPage && (
+          <Button variant={"ghost"} onClick={() => setPage(page + 1)}>
+            <ChevronRight className="w-4 h-4" />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AllPosts;
